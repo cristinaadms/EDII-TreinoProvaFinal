332255b2b6ad6adc13fd5ab2d74c6857ffcde9e0
cristinaadms <cristinadms07@hotmail.com>
2025-01-28 07:34:29 -0300

    alteração 1

--- RBTREE/questao4.c
+++ RBTREE/questao4.c
@@ -2,7 +2,7 @@
 Dada uma árvore rubro-negra que armazena palavras em português e, dentro de cada nó, uma árvore binária que armazena as traduções em inglês e as unidades onde cada tradução é usada (exemplo: "comida" -> "food" em "Restaurante", "Mercado"), implemente uma função em C que, dado a raiz da árvore rubro-negra e uma unidade específica, retorne todas as palavras em português que possuem tradução em inglês utilizada nessa unidade.
 
 
-int exibir_palavras_port(ArvRB *raiz, int unidade){
+void exibir_palavras_port(ArvRB *raiz, int unidade){
 
     if(raiz != NULL){
         for(int i=0; i<raiz->palavrasportugues->num_unidades; i++){
